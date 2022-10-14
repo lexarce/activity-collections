d5d43225e93d6681b86faf64afda405d2bf95d5d
lexarce <lexarce7@gmail.com>
2022-10-13 19:22:29 -0700

    commit and push activity: more on collections

---
+++ src/main/java/com/activity/collections/LinkedListExample.java
@@ -0,0 +1,28 @@
+package com.activity.collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+    public static void main(String[] args) {
+
+        LinkedList<String>  namesLinkedList = new LinkedList<>();
+        namesLinkedList.add("John");
+        namesLinkedList.add("Paul");
+        namesLinkedList.add("George");
+        namesLinkedList.add("Ringo");
+        System.out.println(namesLinkedList.get(2));
+        namesLinkedList.add(1, "Jerry");
+
+        ArrayList<String> namesArrayList = new ArrayList<>();
+        namesArrayList.add("John");
+        namesArrayList.add("Paul");
+        namesArrayList.add("George");
+        namesArrayList.add("Ringo");
+        System.out.println(namesArrayList.get(2));
+        namesArrayList.add(1, "Jerry");
+
+
+    }
+}
---
+++ src/main/java/com/activity/collections/Maps.java
@@ -0,0 +1,34 @@
+package com.activity.collections;
+
+import java.util.HashMap;
+
+public class Maps {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> empIds = new HashMap<>();
+
+        empIds.put("John", 12345);
+        empIds.put("Carl", 54321);
+        empIds.put("Jerry", 8675309);
+
+        System.out.println(empIds);
+
+        System.out.println(empIds.get("Carl"));
+
+        System.out.println(empIds.containsKey("George"));
+
+        System.out.println(empIds.containsValue(8675309));
+
+        empIds.put("John", 98765);
+        System.out.println(empIds);
+
+        empIds.replace("Kramer", 777);
+        System.out.println(empIds);
+
+        empIds.putIfAbsent("Steve", 222);
+        System.out.println(empIds);
+
+        empIds.remove("Steve");
+        System.out.println(empIds);
+    }
+}
